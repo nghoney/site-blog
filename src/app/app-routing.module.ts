e1dea6a6742bb0fd60399579bbd266d529b3391e
Edward <crazybber@outlook.com>
2020-03-09 16:34:09 +0800

    add pagenotfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 // 路由配置
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
- // { path: '**', component: PageNotFoundComponent }
+  { path: '**', loadChildren: () => import('./pagenotfound/pagenotfound.module').then(m => m.PagenotfoundModule) },
 ];
 
 // forRoot()-- 在应用的顶级配置路由器，提供路由需要的服务提供商和指令，基于浏览器当前URL执行首次导航
