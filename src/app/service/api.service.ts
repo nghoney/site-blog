7719d418307c619a26b429778820364a50e6b1f7
Edward <chenlulei@gmail.com>
2020-03-10 21:17:05 +0800

    use HttpClient

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,29 +1,51 @@
-import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
-import { environment } from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
-import { Observable, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
-
+import { of, Observable, throwError } from 'rxjs';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { fromFetch } from 'rxjs/fetch';
 const API_URL = environment.api.base;
 
 @Injectable({
-  providedIn: "root" //TODO: figure out how to inject service instead of being at root of app
+  providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) { }
 
   public Get(endpoint: string): Observable<any> {
     return this.http
       .get(API_URL + endpoint)
       .pipe(
         map(response => {
-          var json = response.json();
+          const json = response.toString();
           return json;
         })
       )
       .pipe(catchError(this.handleError));
   }
+
+  // public Fetch(endpoint: string): Observable<Response> {
+
+  //   return fromFetch(API_URL + endpoint).pipe(
+  //     switchMap(response => {
+  //       if (response.ok) {
+  //         // OK return data
+  //         return response.json();
+  //       } else {
+  //         // Server is returning a status requiring the client to try something else.
+  //         return of({ error: true, message: `Error ${response.status}` });
+  //       }
+  //     }),
+  //     catchError(err => {
+  //       // Network or other error, handle appropriately
+  //       console.error(err);
+  //       return of({ error: true, message: err.message });
+  //     })
+  //   );
+
+  // }
+
 
   public Post(endpoint: string, data: any): Observable<any> {
     return this.http
@@ -39,7 +61,7 @@ export class ApiService {
 
   //handle any error encounted while sending http request
   private handleError(error: Response | any) {
-    console.error("ApiService::handleError", error);
+    console.error('ApiService::handleError', error);
     return throwError(error);
   }
 }
