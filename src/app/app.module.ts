bc1d04ce298bade61fbd61473ee56967393f9857
Edward <chenlulei@gmail.com>
2020-03-10 20:13:26 +0800

    format app root modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
-import { Http, HttpModule } from "@angular/http";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
-import { ContactComponent } from "./contact/contact.component";
-import { PastPostsComponent } from "./past-posts/past-posts.component";
-import { ViewPostComponent } from "./view-post/view-post.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { FooterComponent } from "./footer/footer.component";
-import { MastheadComponent } from "./masthead/masthead.component";
-import { PreviewPostComponent } from "./preview-post/preview-post.component";
-import { AuthorPostComponent } from "./author-post/author-post.component";
-import { LoadingComponent } from "./loading/loading.component";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PastPostsComponent } from './past-posts/past-posts.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MastheadComponent } from './masthead/masthead.component';
+import { PreviewPostComponent } from './preview-post/preview-post.component';
+import { AuthorPostComponent } from './author-post/author-post.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -36,11 +36,11 @@ import { LoadingComponent } from "./loading/loading.component";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule,
+    HttpClientModule,
     FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
